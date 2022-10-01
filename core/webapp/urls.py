23b3e86a2844556a02f162107066c7f67e87a35b
Dmitriy Yugay <Dmitrijugaj614@gmail.com>
2022-10-01 15:00:26 +0600

    Обновлены пути urls.py

--- core/webapp/urls.py
+++ core/webapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from .views.base import index
+from .views.create import create
 
 urlpatterns = [
     path('', index, name='index'),
+    path('create', create, name='create'),
 ]
