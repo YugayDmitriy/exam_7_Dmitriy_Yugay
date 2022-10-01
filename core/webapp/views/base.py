ee719b3c649db89ba4b7395d2769265fe279b532
Dmitriy Yugay <Dmitrijugaj614@gmail.com>
2022-10-01 15:01:43 +0600

    СозданÑо представление base.py

--- core/webapp/views/base.py
+++ core/webapp/views/base.py
@@ -3,10 +3,4 @@ from webapp.models import Visitors
 
 
 def index(request):
-    visitors_list = Visitors.objects.all()
-    choices = Visitors.CHOISES
-    context = {
-        'visitors_list': visitors_list,
-        'choices': choices
-    }
-    return render(request, 'index.html', context=context)
+    return render(request, 'index.html')
